b183f7cedad93735970e61c5c84ab96d54a73c9e
Lars <lars633n@edu.easj.dk>
2021-05-19 01:01:28 +0200

    Main page Bindings

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CKS_1._0.Model;
 
 namespace CKS_1._0.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public CombatKarts ck = CombatKarts.Instance;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
